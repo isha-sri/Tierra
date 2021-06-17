f850b390927a621748d4983243dc084695e674b6
Isha Srivastava <ishasrivastava.1206@gmail.com>
2021-06-18 02:12:28 +0530

    auth basic added

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+require('dotenv').config()
 const express =require("express");
 const bodyParser= require("body-parser");
 const ejs = require("ejs");
-
+const passport = require('passport');
+const cookieSession = require('cookie-session')
+require('./passport');
 const app= express();
+
+app.use(cookieSession({
+  name: 'tuto-session',
+  keys: ['key1', 'key2']
+}))
+
+
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get("/", function(req, res){
   res.render("home");
 })
-app.get("/shop/", function(req, res){
-  res.render("shop");
-})
+// app.get("/shop/", function(req, res){
+//   res.render("shop");
+// })
 app.get("/ourteam/", function(req, res){
   res.render("ourteam");
 })
@@ -27,17 +50,29 @@ app.get("/cart/", function(req, res){
 app.get("/sell/", function(req, res){
   res.render("sell");
 })
-app.get("/sellform/", function(req, res){
-  res.render("sellform");
+
+app.get('/shop', (req, res) => res.render('login'))
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+app.get('/good', isLoggedIn, (req, res) =>{
+  res.render("shop",{name:req.user.displayName,pic:req.user.photos[0].value,email:req.user.emails[0].value})
 })
 
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get("/payment/", function(req, res){
-  res.render("payment");
+app.get('/auth/google/login', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  }
+);
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
 })
-app.get("/splashScreen/", function(req, res){
-  res.render("splashScreen");
-})
+
 app.listen("3000", function(req, res){
   console.log("Server running on port 3000");
 })
