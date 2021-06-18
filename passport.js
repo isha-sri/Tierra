d3f854855c386a7da6a3dd9e8f3097686f866364
Isha Srivastava <ishasrivastava.1206@gmail.com>
2021-06-19 02:06:53 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -16,7 +16,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/login",
+    callbackURL: "http://localhost:8080/auth/google/login",
     passReqToCallback:true
   },
   function(request, accessToken, refreshToken, profile, done) {
