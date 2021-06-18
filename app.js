cbffbc983a07ddea98ede926aae884c0390125f8
Isha Srivastava <ishasrivastava.1206@gmail.com>
2021-06-19 02:04:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -292,6 +292,6 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
