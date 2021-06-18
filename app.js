59af4e2128134f674c809a01aaf4332a58be2e2a
Isha Srivastava <ishasrivastava.1206@gmail.com>
2021-06-19 01:48:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -292,6 +292,6 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 })
 
-app.listen("3000", function(req, res){
-  console.log("Server running on port 3000");
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
